32baf78d64835d24f49939f999a6738d3a5b9e5f
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2021-11-17 12:11:22 +0300

    Add project files.

---
+++ Guardzilla/DiscordBot.cs
@@ -0,0 +1,24 @@
+﻿using DSharpPlus;
+using Microsoft.Extensions.Hosting;
+
+namespace Guardzilla;
+
+internal class DiscordBot : IHostedService
+{
+    private readonly DiscordClient discordClient;
+
+    public DiscordBot(DiscordClient discordClient)
+    {
+        this.discordClient = discordClient;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await discordClient.ConnectAsync();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await discordClient.DisconnectAsync();
+    }
+}
---
+++ Guardzilla/Program.cs
@@ -0,0 +1,36 @@
+﻿using DSharpPlus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Guardzilla;
+
+internal class Program
+{
+    private static async Task Main()
+    {
+        var hostBuilder = new HostBuilder()
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var token = config.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false).Build().GetSection("token").Value;
+
+                context.Properties.Add("token", token);
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddTransient(provider => new DiscordConfiguration()
+                {
+                    Token = (string)context.Properties["token"],
+                    TokenType = TokenType.Bot
+                });
+
+                services.AddTransient(provider => new DiscordClient(provider.GetRequiredService<DiscordConfiguration>()));
+
+                services.AddHostedService<DiscordBot>();
+            })
+            .UseConsoleLifetime();
+
+        await hostBuilder.RunConsoleAsync();
+    }
+}
